85a0049ab2d6cd527cf0088d54e3183e93f4eea2
Egor Laptev <laptencio@yandex.ru>
2023-01-04 20:26:27 +0400

    Task completed

---
+++ src/main/java/Post.java
@@ -0,0 +1,26 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class Post {
+
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final int upvotes;
+
+    public Post(
+            @JsonProperty ("id") String id,
+            @JsonProperty ("text") String text,
+            @JsonProperty ("type") String type,
+            @JsonProperty ("user") String user,
+            @JsonProperty ("upvotes") int upvotes
+    ){
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+}
